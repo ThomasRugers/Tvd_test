bf151ecb7787a0ab8e64aef8db2eea1a82561bca
ThomasRugers <thomasrugers@gmail.com>
2024-08-29 15:14:17 +0200

    Stupid comment has been deleted

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from classmodule import *
 
-# Press the green button in the gutter to run the script.
 def main():
     with open(r"C:\Users\Thomas Rugers\PycharmProjects\Tvd_test\data_Kruidenlaan_2024-01-01-2024-01-05.pkl", 'rb') as file:
         loaded_data = pickle.load(file)
